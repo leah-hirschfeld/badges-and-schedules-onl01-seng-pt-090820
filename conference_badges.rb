581dcdcf5534ac177e3a52d5679d157c884c7612
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-11 23:55:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   arr = []
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each {|i| arr << badge_maker(i)}
   arr
 end
