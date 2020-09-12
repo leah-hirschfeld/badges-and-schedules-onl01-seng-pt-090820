7f77c3eb880142bd71fd8527ec7bd27b3a33de83
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 00:16:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,8 @@ def assign_rooms(attendees)
   arr
 end
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
 
